a42a3ebd285e0f2b0e893d92ad85db5a8f7de9fb
Andrés Pereira de Lucena <apereira@alabs.org>
2018-12-18 12:43:34 +0100

    SMS: changes from to CatEnComu

--- lib/sms.rb
+++ lib/sms.rb
@@ -4,7 +4,11 @@ module SMS
       case Rails.env
       when "staging", "production"
         sms = Esendex::Account.new
-        sms.send_message( to: to, body: "El teu codi d'activació per a Catalunya en Comú és #{code}")
+        sms.send_message(
+          from: "CatEnComu",
+          to: to,
+          body: "El teu codi d'activació per a Catalunya en Comú és #{code}"
+        )
       when "development", "test"
         Rails.logger.info "ACTIVATION CODE para #{to} == #{code}"
       else
