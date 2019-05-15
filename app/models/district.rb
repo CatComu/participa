beb4778cca62b9c1e14824cfb61a28d40421edf7
Mijail Rondon <mijail@riseup.net>
2019-05-15 08:43:31 -0500

    Add polymorphic to districts and veguerias

--- app/models/district.rb
+++ app/models/district.rb
@@ -1,3 +1,4 @@
 class District < ApplicationRecord
   belongs_to :vegueria
+  has_many :groups, as: :territory
 end
--- app/models/vegueria.rb
+++ app/models/vegueria.rb
@@ -1,3 +1,4 @@
 class Vegueria < ApplicationRecord
   belongs_to :province
+  has_many :groups, as: :territory
 end
