e8a22e242538600cfad79585cb491659ace958f3
Mijail Rondon <mijail@riseup.net>
2019-05-14 22:54:02 -0500

    Add polymorphic associations for groups

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,13 +1,28 @@
 class Group < ApplicationRecord
+  belongs_to :territory, polymorphic: true
+
   after_create :add_default_position
   has_many :positions, dependent: :destroy
   has_many :users, through: :positions
 
   validates :name, presence: true
   validates :location_type, presence: true, if: lambda { |u| u.has_location? }
-  validates :space_type, presence: true, if: lambda { |u| u.has_space? }
 
-  enum location_type: [:territory, :municipality, :entity, :comarcal, :provincial]
+  enum location_type: [:province, :vegueria, :district]
+
+  attr_accessor :territory_holder
+
+  def territory_holder
+    "#{territory.class.to_s.downcase}-#{territory_id}"
+  end
+
+  def territory_holder=(data)
+    if data.present?
+      data = data.split("-")
+      self.territory_type = data[0].capitalize
+      self.territory_id = data[1]
+    end
+  end
 
   private
   
