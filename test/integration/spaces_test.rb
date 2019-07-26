04696ee8596609be4b334583fb7db8456304b0fa
Mijail Rondon <mijail@riseup.net>
2019-07-26 02:05:07 -0500

    Fix link text in spaces test

--- test/integration/spaces_test.rb
+++ test/integration/spaces_test.rb
@@ -15,7 +15,7 @@ class SpacesTest < ActionDispatch::IntegrationTest
     visit spaces_path
     assert_content position.territory.name
     assert_content position.name
-    click_link "Download"
+    click_link "Descargar"
     csv = CSV.parse(page.body, headers: true)
     assert csv
     assert_equal 10, csv.size
